b0bceb654eb4f0ef86e83eb564280da7596d1f00
Zeynep106 <duyarzeynep104@gmail.com>
2025-04-24 14:37:58 +0300

    Update index.php

--- FirstWeek/templates/index.php
+++ FirstWeek/templates/index.php
@@ -1,1 +1,16 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+  <meta charset="UTF-8">
+  <title>Ziyaretçi Takip Sistemi</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Ziyaretçi Takip Sistemine Hoş Geldiniz</h1>
+  <nav>
+    <a href="login.php">Giriş Yap</a> |
+    <a href="register.php">Kayıt Ol</a>
+  </nav>
+</body>
+</html>
 
